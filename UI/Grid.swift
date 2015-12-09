94de3eddcb941784c9e2c37c60feb96a6453d635
hkgumbs <h.kofigumbs@gmail.com>
2015-12-09 11:15:44 -0600

    Update Grid for internal consistency

--- UI/Grid.swift
+++ UI/Grid.swift
@@ -6,10 +6,13 @@ public struct Grid<T: Equatable>: CollectionType {
 
     private let contents: [Int: T]
 
-    public init(dimmension:Int, contents: (Int, T)...) {
-        self.contents = convertTuplesToDictionary(contents)
+    public init(dimmension:Int, contents tuples: (Int, T)...) {
+        let endIndex = dimmension * dimmension
+        let contents = convertTuplesToDictionary(tuples, bounds: 0...endIndex)
+
+        self.endIndex = endIndex
         self.dimmension = dimmension
-        self.endIndex = dimmension * dimmension
+        self.contents = contents
         self.isEmpty = contents.isEmpty
     }
 
@@ -19,8 +22,8 @@ public struct Grid<T: Equatable>: CollectionType {
 
 }
 
-private func convertTuplesToDictionary<K, V>(tuples: [(K, V)]) -> [K: V] {
+private func convertTuplesToDictionary<K, V>(tuples: [(K, V)], bounds: Range<K>) -> [K: V] {
     var dictionary: [K: V] = Dictionary()
-    tuples.forEach({ dictionary[$0.0] = $0.1 })
+    tuples.filter({ bounds.contains($0.0) }).forEach({ dictionary[$0.0] = $0.1 })
     return dictionary
 }
--- UITests/GridTest.swift
+++ UITests/GridTest.swift
@@ -5,26 +5,38 @@ class GridTest: XCTestCase {
 
     func testCreatesEmptyGrid() {
         let grid: Grid<String> = Grid(dimmension: 3)
+
         XCTAssertTrue(grid.isEmpty)
     }
 
     func testCreatesGridWithOneCell() {
         let grid: Grid<String> = Grid(dimmension: 3, contents: (0, "Cell 0"))
+
         XCTAssertFalse(grid.isEmpty)
         XCTAssertEqual(grid[0], "Cell 0")
     }
 
     func testCreatesGridWithTwoCells() {
-        let grid: Grid<String> = Grid(dimmension: 3, contents: (0, "Cell 0"), (15, "Cell 15"))
+        let grid: Grid<String> = Grid(dimmension: 3, contents: (0, "Cell 0"), (8, "Cell 8"))
+
         XCTAssertFalse(grid.isEmpty)
         XCTAssertEqual(grid[0], "Cell 0")
-        XCTAssertEqual(grid[15], "Cell 15")
+        XCTAssertEqual(grid[8], "Cell 8")
     }
 
     func testNonInitializedValuesAreNil() {
         let grid: Grid<String> = Grid(dimmension: 3, contents: (1, "Cell 1"))
+
         XCTAssertEqual(grid[0], nil)
         XCTAssertEqual(grid[1], "Cell 1")
+    }
+
+    func testCannotSetValuesOutOfBounds() {
+        let grid: Grid<String> = Grid(dimmension: 3, contents: (-1, "Cell -1"), (15, "Cell 15"))
+
+        XCTAssertTrue(grid.isEmpty)
+        XCTAssertNil(grid[-1])
+        XCTAssertNil(grid[15])
     }
 
 }
