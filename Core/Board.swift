d753959d9e7afc1f5bef2881221fe43e4953d38c
hkgumbs <h.kofigumbs@gmail.com>
2015-12-09 18:54:25 -0600

    Use functional style in Core.Board

--- Core/Board.swift
+++ Core/Board.swift
@@ -1,12 +1,15 @@
 import UI
 
 func markBoard<T>(grid: Grid<T>, position: Int, team: T) -> Grid<T> {
-    var marks = Dictionary<Int, T>()
-    grid.enumerate().forEach({ marks[$0.0] = $0.1 })
-    marks[position] = team
-    return Grid(dimmension: grid.dimmension, contents: marks)
+    let marks = grid.enumerate().reduce(Dictionary<Int, T>(), combine: buildDict)
+    return Grid(dimmension: grid.dimmension, contents: buildDict(marks, entry: (position, team)))
 }
 
 func isBoardFull<T>(grid: Grid<T>) -> Bool {
     return grid.filter({ $0 != nil }).count == grid.count
+}
+
+private func buildDict<K: Hashable, V>(var dict: [K: V], entry: (K, V?)) -> [K:V] {
+    dict[entry.0] = entry.1
+    return dict
 }
