406c126ad25b3526da40f5d66d35495a37fb9cf6
hkgumbs <h.kofigumbs@gmail.com>
2015-12-14 12:33:08 -0600

    Add cli option for four-by-four

--- CoreTests/ConfigurationTest.swift
+++ CoreTests/ConfigurationTest.swift
@@ -6,18 +6,33 @@ class ConfigurationTest: XCTestCase {
     let playerOne = StubPlayer(team: "X")
     let playerTwo = StubPlayer(team: "O")
 
-    func testConfigurationSetsPlayers() {
-        let config = Configuration(players: (playerOne, playerTwo), args: ["program-name"])
+    func testConfigurationSetsDefaultsWithNoArgs() {
+        let config = Configuration(players: (playerOne, playerTwo), args: [])
 
         XCTAssertEqual(config.players.first.team, playerOne.team)
         XCTAssertEqual(config.players.second.team, playerTwo.team)
+        XCTAssertEqual(config.board, Board<String>(dimmension: 3))
     }
 
     func testConfigurationReversesPlayers() {
-        let config = Configuration(players: (playerOne, playerTwo), args: ["program-name", "--reverse"])
+        let config = Configuration(players: (playerOne, playerTwo), args: ["--reverse"])
 
         XCTAssertEqual(config.players.first.team, playerTwo.team)
         XCTAssertEqual(config.players.second.team, playerOne.team)
+    }
+
+    func testConfigurationSetsBoardDimmension() {
+        let config = Configuration(players: (playerOne, playerTwo), args: ["--four"])
+
+        XCTAssertEqual(config.board, Board<String>(dimmension: 4))
+    }
+
+    func testConfigurationCombinesOptions() {
+        let config = Configuration(players: (playerOne, playerTwo), args: ["--reverse", "--four"])
+
+        XCTAssertEqual(config.players.first.team, playerTwo.team)
+        XCTAssertEqual(config.players.second.team, playerOne.team)
+        XCTAssertEqual(config.board, Board<String>(dimmension: 4))
     }
 
 }
