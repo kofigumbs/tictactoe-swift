7e76a95a501dbdec61a78a1173db17632b55bb7c
Kofi Gumbs <h.kofigumbs@gmail.com>
2016-10-28 08:28:45 -0600

    Collapse Grid into Board

--- Sources/Core/Board.swift
+++ Sources/Core/Board.swift
@@ -1,6 +1,31 @@
-public typealias Board<T: Hashable> = Grid<T>
+public struct Board<T: Hashable>: Collection, Equatable {
 
-extension Grid: Equatable {
+    public let startIndex = 0
+    public let endIndex: Int
+    public let dimmension: Int
+    public let isEmpty: Bool
+    public let contents: [Int:T]
+
+    public init(dimmension:Int, contents: [Int:T] = Dictionary()) {
+        var contents = contents
+        let endIndex = dimmension * dimmension
+
+        contents.bound(startingWith: startIndex, endingWith: endIndex)
+
+        self.endIndex = endIndex
+        self.dimmension = dimmension
+        self.contents = contents
+        self.isEmpty = contents.isEmpty
+    }
+
+    public subscript(index: Int) -> T? {
+        return contents[index]
+    }
+
+    public func index(after i: Int) -> Int {
+        return i + 1
+    }
+
     var isFull: Bool { return flatMap({ $0 }).count == count }
 
     func marked(at position: Int, with team: T) -> Board<T> {
@@ -10,7 +35,9 @@ extension Grid: Equatable {
     }
 
     func availableSpaces() -> [Int] {
-        return enumerated().filter({ $0.element == nil }).map({ $0.offset })
+        return enumerated()
+            .filter { $0.element == nil }
+            .map { $0.offset }
     }
 
     public static func ==<T: Hashable>(lhs: Board<T>, rhs: Board<T>) -> Bool {
@@ -18,4 +45,10 @@ extension Grid: Equatable {
     }
 }
 
-
+private extension Dictionary where Key: Integer {
+    func bounded(startingWith start: Key, endingWith end: Key) {
+        self.keys
+            .filter({ $0 >= end || $0 < start })
+            .forEach({ self.removeValue(forKey: $0) })
+    }
+}
