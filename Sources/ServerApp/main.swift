41413bfb4f52ae8979358ea124009e6ef4f76316
Kofi Gumbs <h.kofigumbs@gmail.com>
2016-11-05 20:39:49 -0600

    Switch to WebSocket with real UI

--- Sources/ServerApp/main.swift
+++ Sources/ServerApp/main.swift
@@ -2,31 +2,27 @@ import TicTacToe
 import Vapor
 
 
+let solver = Solver(team: false, opponent: true)
 let drop = Droplet()
-let ui = ServerUI()
-let players = (Human(team: true, ui: ui), Solver(team: false, opponent: true))
-let simulation = Simulation(ui: ui, players: players, args: CommandLine.arguments)
-let message = (
-   boardNotAvailable: "Sorry, the board is not available yet.",
-   notYourTurn: "Sorry, it's not your turn.",
-   success: "Success!"
-)
 
-func encode(bool: Bool?) -> String? {
-    return bool.map { $0 ? "X" : "O" }
+drop.socket("game") { _, ws in
+    let ui = ServerUI(update: ws.send)
+    let human = Human(team: true, ui: ui)
+    let simulation = Simulation(ui: ui, players: (human, solver), args: [])
+
+    ws.onText = { ws, text in
+        let json = try JSON(bytes: Array(text.utf8))
+
+        if let move = json.object?["move"]?.int {
+            ui.send(move: move)
+        }
+    }
+
+    simulation.start()
 }
 
-drop.get("board") { _ in
-    guard let board = ui.lastBoard else { return message.boardNotAvailable }
-    return try JSON(node: board.map(encode)).makeBytes().string
+drop.get("/") { _ in
+    return try drop.view.make("index.html")
 }
 
-drop.get("move", Int.self) { _, param in
-    guard ui.waitingForMove else { return message.notYourTurn }
-    ui.move(param)
-    return message.success
-}
-
-
-simulation.start()
 drop.run()
